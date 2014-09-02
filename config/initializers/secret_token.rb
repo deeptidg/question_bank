6ded213b51ac1b791c9aa45cb6e61726f326f4d2
deeptidg <deepti_goteti@yahoo.com>
2014-09-02 19:44:32 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuestionBank::Application.config.secret_key_base = 'a993cbed7c3a9b35122ea446c50d96916e67c0d9d43d73c3a70445320f995489bd12bb8d19a51d1dc14c72d5318fc14a30e57811cdca2a0b1b0026615a52be94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuestionBank::Application.config.session_store :cookie_store, key: '_question_bank_session'
