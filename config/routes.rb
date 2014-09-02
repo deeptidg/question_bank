8e7fafddb2293bdc2529f01567e7099752308a74
deeptidg <deepti_goteti@yahoo.com>
2014-09-02 19:46:04 -0400

    Generated controller Questions with action index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 QuestionBank::Application.routes.draw do
+  get "questions/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
