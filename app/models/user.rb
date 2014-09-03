cc7488f2a4a3ae9428d304c2cfb211bf34700232
deeptidg <deepti_goteti@yahoo.com>
2014-09-02 20:09:43 -0400

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 	
-	has_many :questions
-	
 	has_secure_password
 
+	validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..12 }, 
+	          format: { with: /\A[a-z][a-z0-9]*$\Z/, message: 'can only contain lower case letters and numbers'} 
+	validates :password, length: { in: 4..8 }
+	validates :password_confirmation, length: { in: 4..8 }
 end
 
